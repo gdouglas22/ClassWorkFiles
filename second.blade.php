446eb8b9761300bf7f3894afedcc50a78e8952ff
gdouglas22 <103975395+gdouglas22@users.noreply.github.com>
2024-03-24 11:24:36 +0300

    Create second.blade.php

---
+++ second.blade.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="ru">
+  <head>
+    <meta charset="utf-8">
+    <title>Дела</title>
+    
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+  </head>
+  <body class="bg-light">
+    <div class="container py-4">
+      <h1 class="mb-5">Сделать сегодня</h1>
+      <ul class="list-group">
+        <li class="list-group-item">Задача1</li>
+        <li class="list-group-item">Задача2</li>
+        <li class="list-group-item">Задача3</li>
+        <li class="list-group-item">Задача4</li>
+        <li class="list-group-item">Задача5</li>
+      </ul>
+    </div>
+  </body>
+</html>
